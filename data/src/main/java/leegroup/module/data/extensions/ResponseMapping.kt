e654fe7974f7b91acd6dcaef1b9f531afd79825c
Long Do <Longdt57@gmail.com>
2024-12-29 13:06:12 +0900

    Remove Todo

--- data/src/main/java/leegroup/module/data/extensions/ResponseMapping.kt
+++ data/src/main/java/leegroup/module/data/extensions/ResponseMapping.kt
@@ -55,10 +55,7 @@ private fun Throwable.mapError(): Throwable {
 private fun parseErrorResponse(response: Response<*>?): ErrorResponse? {
     val jsonString = response?.errorBody()?.string() ?: return null
     return try {
-        Json.decodeFromString<ErrorResponse>(
-            // Todo ignore var
-            jsonString
-        )
+        Json.decodeFromString<ErrorResponse>(jsonString)
     } catch (ex: SerializationException) {
         null
     } catch (ex: IllegalArgumentException) {
